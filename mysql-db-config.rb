1cd04b89cd13b7ef6c586b37749e6a4623af1b4e
Peter Haza <peter.haza@gmail.com>
2010-02-04 09:21:21 +0100

    Add mysql config script

---
+++ mysql-db-config.rb
@@ -0,0 +1,50 @@
+
+# Create a development/test/prod database for a new rails template. 
+# Apply to a new Rails application using  rails:template rake task 
+# and supplying LOCATION environment variable :                      
+#                                                                    
+#        rake rails:template LOCATION=/path/to/this_template         
+
+# Snatched from: http://github.com/lgs/rails-templates
+
+dbname   = ask("\nCreate a new MySQL DB named :")
+mysqlusr = ask("\nwith user :")
+passwd   = ask("\nand password :")
+
+file 'config/database.yml', <<-YAML
+development:
+  adapter: mysql
+  encoding: utf8
+  reconnect: false
+  database: #{dbname}
+  pool: 5
+  username: #{mysqlusr}
+  password: #{passwd}
+  host: localhost
+test:
+  adapter: mysql
+  encoding: utf8
+  reconnect: false
+  database: #{dbname}_test
+  pool: 5
+  username: #{mysqlusr}
+  password: #{passwd}
+  host: localhost
+production:
+  adapter: mysql
+  encoding: utf8
+  reconnect: false
+  database: #{dbname}
+  pool: 5
+  username: #{mysqlusr}
+  password: #{passwd}
+  host: localhost
+YAML
+
+# Run the migration
+if yes?("\nCreate and migrate databases now? (y/n)")
+  # run "mysqladmin -u #{mysqlusr} create #{dbname}" -p #{passwd}
+  # run "mysqladmin -u #{mysqlusr} create #{dbname}_test" -p #{passwd}
+  rake("db:create:all")
+  rake("db:migrate")
+end
